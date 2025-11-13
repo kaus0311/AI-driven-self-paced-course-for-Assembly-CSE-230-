8c5a1ba8e146373960f91a43fdce523878bd14ee
CharlesSheelam <csheelam@asu.edu>
2025-11-13 13:54:56 -0700

    feat: mastery quiz

---
+++ frontend/app/module/[id]/mastery/resources.ts
@@ -0,0 +1,208 @@
+export const RESOURCES: Record<string, Record<string, string[]>> = {
+  "1": {
+    "1": [
+      "https://www.cse.iitd.ac.in/~srsarangi/archbook/moore.pdf",
+      "https://en.wikipedia.org/wiki/Moore%27s_law"
+    ],
+    "2": [
+      "https://csapp.cs.cmu.edu/3e/labs.html",
+      "https://www.geeksforgeeks.org/levels-of-computer-system/"
+    ],
+    "3": [
+      "https://www.cs.virginia.edu/~evans/cs216/guides/cpu-time.html",
+      "https://patterson.pacman.ee.ucla.edu/cpu-performance"
+    ],
+    "4": [
+      "https://www.ecs.umass.edu/ece/koren/architecture/part1/cycles.html",
+      "https://en.wikibooks.org/wiki/MIPS_Assembly/CPI"
+    ],
+    "5": [
+      "https://en.wikipedia.org/wiki/Amdahl%27s_law",
+      "https://www.cs.ucr.edu/~zhiyunq/teaching/cs145/Amdahl.pdf"
+    ],
+    "26": [
+      "https://www.cs.fsu.edu/~zwang/files/cda3100/lecture04.pdf",
+      "https://cs.stanford.edu/people/eroberts/courses/cs107/Performance.pdf"
+    ],
+    "27": [
+      "https://www.geeksforgeeks.org/difference-throughput-response-time/",
+      "https://cs.stackexchange.com/questions/19062/definition-of-response-time"
+    ],
+    "28": [
+      "https://www.geeksforgeeks.org/difference-throughput-response-time/",
+      "https://queue.acm.org/detail.cfm?id=1772130"
+    ],
+    "29": [
+      "https://en.wikipedia.org/wiki/Amdahl%27s_law",
+      "https://pages.cs.wisc.edu/~remzi/Classes/537/Spring2018/Notes/02-performance.pdf"
+    ],
+    "30": [
+      "https://csapp.cs.cmu.edu/",
+      "https://www.csee.umbc.edu/portal/help/architecture/memory.html"
+    ]
+  },
+
+  "2": {
+    "6": [
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf",
+      "https://www.geeksforgeeks.org/mips-instruction-formats/"
+    ],
+    "7": [
+      "https://www.csee.umbc.edu/portal/help/architecture/alu.html",
+      "https://www.tutorialspoint.com/mips-alu-operations"
+    ],
+    "8": [
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf",
+      "https://stackoverflow.com/questions/25407519/mips-load-store-instructions"
+    ],
+    "9": [
+      "https://www.geeksforgeeks.org/memory-addressing-mips-instructions/",
+      "https://chortle.ccsu.edu/assemblytutorial/Chapter-12/ass12_3.html"
+    ],
+    "10": [
+      "https://stackoverflow.com/questions/36400453/mips-offsets-and-addressing",
+      "https://www.cs.uwm.edu/classes/cs315/Bacon/Lecture/HTML/ch05s07.html"
+    ],
+    "31": [
+      "https://www.tutorialspoint.com/computer_logical_organization/memory_hierarchy.htm",
+      "https://inst.eecs.berkeley.edu/~cs61c/su20/lectures/"
+    ],
+    "32": [
+      "https://www.gatevidyalay.com/memory-hierarchy-in-computer-organization/",
+      "https://people.cs.pitt.edu/~childers/CS441/lectures/cs441-3-mips.pdf"
+    ],
+    "33": [
+      "https://www.cs.tufts.edu/comp/140/lectures/11/MIPS-ALU.pdf",
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/07-alufunctions.pdf"
+    ],
+    "34": [
+      "https://www.geeksforgeeks.org/addressing-modes-in-computer-architecture/",
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf"
+    ]
+  },
+
+  "3": {
+    "11": [
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf",
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/branch.html"
+    ],
+    "12": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/jump.html",
+      "https://chortle.ccsu.edu/AssemblyTutorial/Chapter-14/ass14_2.html"
+    ],
+    "13": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/04-machinecode.pdf",
+      "https://www.csee.umbc.edu/portal/help/architecture/mips.html"
+    ],
+    "14": [
+      "https://www.geeksforgeeks.org/mips-branch-instructions/",
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf"
+    ],
+    "15": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/04-machinecode.pdf",
+      "https://www.tutorialspoint.com/mips-machine-code-encoding"
+    ],
+    "35": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/jump.html",
+      "https://www.cs.tufts.edu/comp/140/lectures/11/MIPS-branching.pdf"
+    ],
+    "36": [
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf",
+      "https://www.geeksforgeeks.org/overflow-in-addition/"
+    ],
+    "37": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/branch.html",
+      "https://www.cs.uic.edu/~jbell/CourseNotes/CompOrg/MIPS.Branching.html"
+    ],
+    "38": [
+      "https://www.geeksforgeeks.org/mips-r-i-j-instruction-formats/",
+      "https://chortle.ccsu.edu/AssemblyTutorial/Chapter-14/ass14_5.html"
+    ]
+  },
+
+  "4": {
+    "16": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/jal.html",
+      "https://www.tutorialspoint.com/mips-jump-and-link"
+    ],
+    "17": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/09-stackframes.pdf",
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf"
+    ],
+    "18": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/09-stackframes.pdf",
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/stack.html"
+    ],
+    "19": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/arguments.html",
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf"
+    ],
+    "20": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/09-stackframes.pdf",
+      "https://stackoverflow.com/questions/34922108/mips-stack-pointer"
+    ],
+    "41": [
+      "https://www.csee.umbc.edu/portal/help/architecture/mips.html",
+      "https://www.tutorialspoint.com/mips-registers"
+    ],
+    "42": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/arguments.html",
+      "https://pages.cs.wisc.edu/~larus/HP_examples/mips.html"
+    ],
+    "43": [
+      "https://www.cs.umd.edu/class/fall2002/cmsc311/Notes/Mips/jump.html",
+      "https://www.csee.umbc.edu/portal/help/architecture/mips.html"
+    ],
+    "44": [
+      "https://cs61c.org/resources/",
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/09-stackframes.pdf"
+    ],
+    "45": [
+      "https://www.cs.umd.edu/class/spring2020/cmsc216/Lectures/09-stackframes.pdf",
+      "https://stackoverflow.com/questions/25363056/frame-pointer-in-mips"
+    ]
+  },
+
+  "5": {
+    "21": [
+      "https://web.stanford.edu/class/archive/cs/cs107/cs107.1224/lectures/08-Linker-Lectures.pdf",
+      "https://www.geeksforgeeks.org/linker-in-system-programming/"
+    ],
+    "22": [
+      "https://wiki.osdev.org/ELF",
+      "https://linuxhint.com/elf-file-format/"
+    ],
+    "23": [
+      "https://www.geeksforgeeks.org/bss-segment-c-set-1/",
+      "https://refspecs.linuxfoundation.org/elf/elf.pdf"
+    ],
+    "24": [
+      "https://www.bottomupcs.com/linkers.xhtml",
+      "https://web.stanford.edu/class/archive/cs/cs107/cs107.1224/lectures/08-Linker-Lectures.pdf"
+    ],
+    "25": [
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf",
+      "https://www.csee.umbc.edu/portal/help/architecture/memory.html"
+    ],
+    "46": [
+      "https://www.geeksforgeeks.org/memory-segmentation-in-operating-system/",
+      "https://refspecs.linuxfoundation.org/elf/elf.pdf"
+    ],
+    "47": [
+      "https://www.ibm.com/docs/en/aix/7.2?topic=concepts-static-vs-dynamic-libraries",
+      "https://www.geeksforgeeks.org/static-vs-dynamic-libraries/"
+    ],
+    "48": [
+      "https://www.geeksforgeeks.org/loader-in-operating-system/",
+      "https://www.bottomupcs.com/linkers.xhtml"
+    ],
+    "49": [
+      "https://www.geeksforgeeks.org/position-independent-code/",
+      "https://refspecs.linuxfoundation.org/elf/elf.pdf"
+    ],
+    "50": [
+      "https://stackoverflow.com/questions/2326763/what-is-the-global-pointer-gp-used-for",
+      "https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Guide.pdf"
+    ]
+  }
+};
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    "app/module/[id]/mastery/page": ["../db/init/questions/**/*"],
+  }
 };
 
 export default nextConfig;
